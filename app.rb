72ac4927836d4299c07288d0e1189f79477de34e
Tanya Hodges <tanyac52003@gmail.com>
2020-07-20 18:56:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,9 @@ class App < Sinatra::Base
   end 
   
   get '/multiply/:num1/:num2' do
-    "num1 * num2"
+    @num1 = params[:num1].to_i 
+    @num2 = params[:num2].to_i 
+    @num1 * @num2 
   end 
   
 end
